1ff73c90cf6385de54ab63139d33318a00e66e25
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2021-09-08 18:21:22 +0300

    Delete Moon.js

--- src/components/spaceComponents/aroundEarth/Moon.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import { useTexture } from "@react-three/drei";
-import { useSpring, animated } from "@react-spring/three";
-
-const Moon = () => {
-  return <animated.mesh> </animated.mesh>;
-};
-
-export default Moon;
