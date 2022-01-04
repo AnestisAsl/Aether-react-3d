f19eead188c7f217fd199afe840f5a6ea1ff22fd
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2022-01-04 16:18:19 +0200

    Add files via upload

---
+++ src/components/spaceComponents/AroundSaturn.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import RingsOfSaturn from "./aroundSaturn/RingsOfSaturn";
+import Titan from "./aroundSaturn/Titan";
+const AroundSaturn = () => {
+  return (
+    <>
+      <RingsOfSaturn />;
+      <Titan />
+    </>
+  );
+};
+
+export default AroundSaturn;
