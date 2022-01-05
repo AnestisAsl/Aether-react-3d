fe4363d15d81a7f7ef1923ccfc2eb158f87b85d9
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2022-01-05 23:47:20 +0200

    Add files via upload

---
+++ src/components/spaceComponents/AroundVenus.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import VenusArmosphere from "./aroundVenus/VenusAtmosphere";
+
+const AroundVenus = () => {
+  return <VenusArmosphere />;
+};
+
+export default AroundVenus;
