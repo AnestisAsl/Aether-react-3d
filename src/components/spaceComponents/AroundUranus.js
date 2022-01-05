f1e1949491db6dbf6b81be01eded429403b37492
Anestis Aslanidis <anestisaslanidis1997@gmail.com>
2022-01-05 23:47:34 +0200

    Add files via upload

---
+++ src/components/spaceComponents/AroundUranus.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Titania from "./aroundUranus/Titania";
+import Umbriel from "./aroundUranus/Umbriel";
+
+const AroundUranus = () => {
+  return (
+    <>
+      <Umbriel />
+      <Titania />
+    </>
+  );
+};
+
+export default AroundUranus;
